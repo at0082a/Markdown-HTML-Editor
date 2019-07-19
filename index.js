fc3213099b996f73d95509fc56756eaa6ad6f0a3
alex <you@example.com>
2019-07-19 03:49:14 +0000

    added ability to have a second user interact with document with sharejs and redis

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* server.js */
+
 var express = require('express');
 var app = express();
 
@@ -11,8 +13,22 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
   res.render('pad');
 });
+app.get('/(:id)', function(req, res) {
+  res.render('pad');
+});
+
+// get sharejs dependencies
+var sharejs = require('share');
+require('redis');
+
+// options for sharejs 
+var options = {
+  db: {type: 'redis'},
+};
+
+// attach the express server to sharejs
+sharejs.server.attach(app, options);
 
 // listen on port 8000 (for localhost) or the port defined for heroku
 var port = process.env.PORT || 8000;
-console.log(`app is listening on port ${port}`)
 app.listen(port);
